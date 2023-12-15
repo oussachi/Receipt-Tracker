d71694f52d37ef7e1fb8dfc07f9638b88ec679e6
oussama di <ousstheouss@outlook.com>
2023-12-15 11:20:17 +0100

    Added urls docs

--- receipts/urls.py
+++ receipts/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_hello, name='home'),
-    path('login/', views.loginView, name='login'),
-    path('register/', views.registerView, name='register'),
-    path('logout/', views.logoutView, name='logout'),
-    path('receipts/', views.getReceipts, name='my_receipts'),
-    path('receipt/<int:id>', views.getReceipt, name='receipt'),
-    path('receipts/add', views.addReceipt, name='add_receipt'),
-    path('receipt/<int:id>/update', views.updateReceipt, name='update_receipt'),
-    path('receipt/<int:id>/delete', views.deleteReceipt, name='delete_receipt')
+    path('', views.say_hello, name='home'),                                         # Home page
+    path('login/', views.loginView, name='login'),                                  # Login page
+    path('register/', views.registerView, name='register'),                         # Registration page
+    path('logout/', views.logoutView, name='logout'),                               # Logout Endpoint
+    path('receipts/', views.getReceipts, name='my_receipts'),                       # User's receipts page
+    path('receipt/<int:id>', views.getReceipt, name='receipt'),                     # User's speicific receipt page
+    path('receipts/add', views.addReceipt, name='add_receipt'),                     # Add receipt page
+    path('receipt/<int:id>/update', views.updateReceipt, name='update_receipt'),    # Update receipt page
+    path('receipt/<int:id>/delete', views.deleteReceipt, name='delete_receipt')     # Delete receipt page
 ]
